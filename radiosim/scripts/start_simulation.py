02fb4280ffed069651759d73fe934910516e9c5c
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-09-29 16:57:55 +0200

    start simulation structure

--- radiosim/scripts/start_simulation.py
+++ radiosim/scripts/start_simulation.py
@@ -1,1 +1,30 @@
 import numpy as np
+import toml
+import click
+
+
+@click.command()
+@click.argument("configuration_path", type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    "--mode",
+    type=click.Choice(
+        [
+            "simulate",
+            "overview",
+        ],
+        case_sensitive=False,
+    ),
+    default="train",
+)
+def main(configuration_path, mode):
+    config = toml.load(configuration_path)
+    sim_conf = read_config(config)
+
+if mode == "simulate":
+
+
+if mode == "overview":
+
+
+if __name__ == "__main__":
+    main()
