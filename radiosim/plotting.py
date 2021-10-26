8066911f9ef0e754c37fa884676d10d8a7a65786
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-10-26 12:19:26 +0200

    add overview plot

--- radiosim/plotting.py
+++ radiosim/plotting.py
@@ -1,4 +1,8 @@
+import re
+import h5py
+import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 def plot_distribution(img):
@@ -49,3 +53,40 @@ def plot_single_example(img, source_list):
     plt.xlabel("pixels")
     plot_legend()
     plt.tight_layout()
+
+
+def plot_overview(f):
+    num_exp = len(f) / 3
+    i = np.random.choice(np.arange(num_exp, dtype="int"), 6, replace=False)
+
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+    ax1.imshow(f["sky" + str(i[0])])
+    ax1.axis("off")
+    ax2.imshow(f["sky" + str(i[1])])
+    ax2.axis("off")
+    ax3.imshow(f["sky" + str(i[2])])
+    ax3.axis("off")
+    ax4.imshow(f["sky" + str(i[3])])
+    ax4.axis("off")
+    ax5.imshow(f["sky" + str(i[4])])
+    ax5.axis("off")
+    ax6.imshow(f["sky" + str(i[5])])
+    ax6.axis("off")
+
+    plt.tight_layout()
+
+
+def create_simulation_overview(sim_conf):
+    path = Path(sim_conf["outpath"])
+    bundles = np.array([x for x in path.iterdir() if re.findall(".h5", x.name)])
+    bundle = np.random.choice(bundles)
+    f = h5py.File(bundle, "r")
+
+    img = f["sky0"]
+    s_list = f["list0"][()]
+    plot_single_example(img, s_list)
+    path_overview = path / "overview"
+    path_overview.mkdir(exist_ok=True)
+    plt.savefig(path_overview / "single_source.pdf")
+    plot_overview(f)
+    plt.savefig(path_overview / "overview.pdf")
