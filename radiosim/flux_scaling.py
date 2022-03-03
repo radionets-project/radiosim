d9ca19d6ec90b30fe22ccb0877d2bf58140e3f99
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2022-03-03 05:24:05 +0100

    add mojave peak flux distribution

---
+++ radiosim/flux_scaling.py
@@ -0,0 +1,16 @@
+from scipy import stats
+
+
+def get_start_amp(scale_type):
+    if scale_type == "mojave":
+        amp_start = draw_from_mojave_dist()
+    return amp_start
+
+
+def draw_from_mojave_dist():
+    """Values from a fit to the peak fluxes distribution in the MOJAVE data archive."""
+    a = 0.8639672251677816
+    b = 47.64189171625089
+    loc = 0.09163404776954732
+    scale = 1892.3881692069087
+    return stats.beta(a, b, loc, scale).rvs(1)
