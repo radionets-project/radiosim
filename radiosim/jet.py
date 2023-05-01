bbf1d9e6dd4647c5a8254bc6bac0eb52b8faa0c1
Arne Poggenpohl <arne.poggenpohl@tu-dortmund.de>
2023-05-01 11:06:19 +0200

    Renaming and deletion for better readability

--- radiosim/jet.py
+++ radiosim/jet.py
@@ -1,5 +1,5 @@
 import numpy as np
-from radiosim.utils import relativistic_boosting, pol2cart, zoom_on_source, zoom_out
+from radiosim.utils import relativistic_boosting, pol2cart
 from radiosim.gauss import twodgaussian
 from radiosim.flux_scaling import get_start_amp
 
@@ -53,7 +53,7 @@ def create_jet(grid, conf):
         beta[1] = np.random.uniform(0, 1)
         y_rotation = np.random.uniform(0, 2 * np.pi)
         z_rotation = np.random.uniform(0, np.pi / 2)
-        extension = np.random.uniform(0, 0.5)
+        expansion = np.random.uniform(0, 0.5)
 
         for i in range(comps):
             # amplitude decreases for more distant components, empirical
@@ -90,7 +90,7 @@ def create_jet(grid, conf):
                     img_size
                     / comps
                     * r_factor
-                    * (i + 1) ** extension
+                    * (i + 1) ** expansion
                     / np.random.uniform(3, 9, size=2)
                 )
             )[::-1]
@@ -136,34 +136,6 @@ def create_jet(grid, conf):
         # creation of the image
         jet_comp = np.array(component_from_list(img_size, amp, x, y, sx, sy, rotation))
         jet_img = np.sum(jet_comp, axis=0)
-
-        # # get values at the edge of the image
-        # edge_list = [
-        #     jet_img[0, :-1],
-        #     jet_img[:-1, -1],
-        #     jet_img[-1, ::-1],
-        #     jet_img[-2:0:-1, 0],
-        # ]
-        # edges = np.concatenate(edge_list)
-        # edge_threshold = 0.01
-        # if edges.max() < edge_threshold:
-        #     # zoom on source to equalize size differences from z-rotation
-        #     jet_img, jet_comp, zoom_factor = zoom_on_source(
-        #         jet_img, jet_comp, max_amp=edge_threshold
-        #     )
-        #     x = img_size / 2 + (x - img_size / 2) * zoom_factor
-        #     y = img_size / 2 + (y - img_size / 2) * zoom_factor
-        #     sx *= zoom_factor
-        #     sy *= zoom_factor
-
-        #     # random zoom out for more variance
-        #     zoom_out_factor = np.random.uniform(1 / 2, 1)  # 1/8: pad eg. 128 -> 1024
-        #     pad_value = (1 / zoom_out_factor - 1) * img_size / 2
-        #     jet_img, jet_comp = zoom_out(jet_img, jet_comp, pad_value=pad_value)
-        #     x = img_size / 2 + (x - img_size / 2) * zoom_out_factor
-        #     y = img_size / 2 + (y - img_size / 2) * zoom_out_factor
-        #     sx *= zoom_out_factor
-        #     sy *= zoom_out_factor
 
         # normalisation
         if conf["scaling"] == "normalize":
