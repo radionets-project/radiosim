ffa3699fcb7248a955bbe299bac1c4287389543b
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-11-17 14:38:01 +0100

    Update color of help message

--- src/radiosim/tools/cli.py
+++ src/radiosim/tools/cli.py
@@ -21,7 +21,7 @@ click.rich_click.COMMAND_GROUPS = {
 
 @click.group(
     help=f"""
-    This is the [dark_orange]radiosim[/]
+    This is the [dark_turquoise]radiosim[/]
     [cornflower_blue]v{__version__}[/] main CLI tool.
     """
 )
